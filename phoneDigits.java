08e530ed8a57795608c6de89c9acf6dffa34e3f4
TrimurthuluPosimsetti <40907951+TrimurthuluPosimsetti@users.noreply.github.com>
2019-03-12 18:19:36 +0530

    Update phoneDigits.java

--- phoneDigits.java
+++ phoneDigits.java
@@ -20,30 +20,25 @@ class Main {
     for(int i=0;i<hm.get(s.charAt(0)).length();i++){
         c[0]=hm.get(s.charAt(0)).charAt(i);
         phoneDigit(c,s,ar,1,1,hm);
-        System.out.println("Hello");
+        //System.out.println("Hello");
     }
     for(int i=0;i<ar.size();i++){
       System.out.println(ar.get(i));
     }
   }
   public static void phoneDigit(char c[],String s,ArrayList<String> ar,int in,int strin,HashMap<Character,String> hm){
-    System.out.println(strin+" "+in);
+    //System.out.println(strin+" "+in);
     for(int i=0;i<s.length();i++){
       c[in]=hm.get(s.charAt(strin)).charAt(i);
-      // for(int j=0;j<s.length();j++){
-      //   System.out.print(c[j]+" ");
-      // }
-      // System.out.println();
       if(in==s.length()-1){
-        //System.out.println(in);
         String v="";
-        for(int j=0;i<s.length();j++){
+        for(int j=0;j<s.length();j++){
             v=v+Character.toString(c[j]);
         }
-        ar.add("abc");
+        ar.add(v);
       }
       else{
-        System.out.println(strin+1+" "+in+1);
+        //System.out.println(strin+1+" "+in+1);
         phoneDigit(c,s,ar,in+1,strin+1,hm);
         c[in+1]=' '; 
       }
